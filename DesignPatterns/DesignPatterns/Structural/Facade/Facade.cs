f7473aa3c56cea312dc991e6a1e5783d259beec5
vasylhulpak <vh@interlogic.com.ua>
2023-07-25 09:58:10 +0300

    Added facade pattern

---
+++ DesignPatterns/DesignPatterns/Structural/Facade/Facade.cs
@@ -0,0 +1,25 @@
+﻿namespace DesignPatterns.Structural.Facade;
+
+public class Facade
+{
+	protected Subsystem1 _subsystem1;
+        
+	protected Subsystem2 _subsystem2;
+
+	public Facade(Subsystem1 subsystem1, Subsystem2 subsystem2)
+	{
+		_subsystem1 = subsystem1;
+		_subsystem2 = subsystem2;
+	}
+        
+	public string Operation()
+	{
+		string result = "Facade initializes subsystems:\n";
+		result += _subsystem1.operation1();
+		result += _subsystem2.operation1();
+		result += "Facade orders subsystems to perform the action:\n";
+		result += _subsystem1.operationN();
+		result += _subsystem2.operationZ();
+		return result;
+	}
+}
---
+++ DesignPatterns/DesignPatterns/Structural/Facade/README.md
@@ -0,0 +1,14 @@
+﻿# Facade
+Facade is a structural design pattern that provides a simplified interface to a library, a framework, or any other complex set of classes.
+## Real-World Analogy
+
+<div align="center">
+
+![img.png](../../Images/facade.png)An example of taking a phone order
+
+</div>
+When you call a shop to place a phone order, an operator is your facade to all services and departments of the shop. The operator provides you with a simple voice interface to the ordering system, payment gateways, and various delivery services.
+
+MIT Licensed
+
+**Copyright © 2023-present Vasyl Hulpak**
