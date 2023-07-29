3d1321a7f048200f6cfa9fbd9069ff234dd66005
vasylhulpak <vh@interlogic.com.ua>
2023-07-29 14:20:41 +0300

    Made small changes on Abstraction class

--- DesignPatterns/DesignPatterns/Structural/Bridge/Abstraction.cs
+++ DesignPatterns/DesignPatterns/Structural/Bridge/Abstraction.cs
@@ -1,14 +1,25 @@
 ﻿namespace DesignPatterns.Structural.Bridge
 {
+	/// <summary>
+	/// Class Abstraction.
+	/// </summary>
 	public class Abstraction
 	{
-		protected IImplementation _implementation;
+		protected readonly IImplementation _implementation;
         
+		/// <summary>
+		/// <inheritdoc cref="Abstraction"/>
+		/// </summary>
+		/// <param name="implementation"></param>
 		public Abstraction(IImplementation implementation)
 		{
 			_implementation = implementation;
 		}
         
+		/// <summary>
+		/// Method Operation.
+		/// </summary>
+		/// <returns>string</returns>
 		public virtual string Operation()
 		{
 			return "Abstract: Base operation with:\n" + 
